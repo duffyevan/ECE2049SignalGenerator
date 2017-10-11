c61de7a4098a90115b178641fc47109474f8e536
Evan Duffy <elduffy@wpi.edu>
2017-10-11 14:38:02 -0400

    Ready to butcher main loop

--- adc.c
+++ adc.c
@@ -4,26 +4,25 @@
 
 extern unsigned int potValue;
 extern unsigned char buttonSetting;
+unsigned int adcmem1 = 0;
+
 #pragma vector=ADC12_VECTOR
 __interrupt void ADC12ISR (void){
-//    switch (buttonSetting){
-//    case 1: // DC
         potValue = ADC12MEM0;
-//        break;
-//    }
+        adcmem1 = ADC12MEM1;
 }
 
 
 
 void setupADC(){
     REFCTL0 &= ~REFMSTR; // get ready to use internal reference
-    ADC12CTL0 = ADC12SHT0_9 | ADC12REFON | ADC12ON; // set for multi sample conversion. use middle of the road sample time. Use internal reference (default 1.5v). Turn the ADC on
-    ADC12CTL1 = ADC12SHP; // pulsemode. sequence of conversions. start address is 0.
+	ADC12CTL0 = ADC12MSC | ADC12SHT0_9 | ADC12REFON | ADC12ON; // set for multi sample conversion. use middle of the road sample time. Use internal reference (default 1.5v). Turn the ADC on
+	ADC12CTL1 = ADC12SHP | ADC12CONSEQ_1 | ADC12CSTARTADD_0; // pulsemode. sequence of conversions. start address is 0.
 
-    P6SEL |= BIT0; // enable A0 instead of P6.0
+    P6SEL |= BIT0 | BIT1; // enable A0 instead of P6.0
 
     ADC12MCTL0 = ADC12SREF_0 | ADC12INCH_0; // Sets the ref to internal reference. Selects temperature diode for channel.
-    //ADC12MCTL1 = ADC12INCH_0 | ADC12EOS; // set the input channel to the pot pin (A0) and set it as the end of the sequence
+    ADC12MCTL1 = ADC12INCH_1 | ADC12EOS; // set the input channel to the pot pin (A0) and set it as the end of the sequence
     enableConversionInterrupt();
 }
 
@@ -41,6 +40,6 @@ int getMeasurement(){
 }
 
 void enableConversionInterrupt(){
-    ADC12IE |= 0x0001; // Set it so it will fire when its done converting MEM0
+    ADC12IE |= 0x0002; // Set it so it will fire when its done converting MEM0
 }
 
